041087ca15ca36ce8c780140d14d3c90db9dbb17
Dhoots94 <dhoots94@gmail.com>
2025-03-25 18:02:13 -0400

    Fix dialog component import case sensitivity

--- src/components/education/ResourceDetailModal.tsx
+++ src/components/education/ResourceDetailModal.tsx
@@ -1,6 +1,6 @@
 // src/components/education/ResourceDetailModal.tsx
 import React, { useState, useEffect } from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '../../components/ui/Dialog';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
 import { EducationalResource, ResourceRatingInfo } from '@/types/education';
