e3a5097955b3858e4e056e1b8c4cdc37fce0c82b
Dhoots94 <dhoots94@gmail.com>
2025-04-24 20:30:57 -0400

    Add root directory wrapper scripts for easier execution

---
+++ fix-dependencies.js
@@ -0,0 +1,8 @@
+/**
+ * Root directory wrapper for fix_missing_dependencies.js
+ * 
+ * This script forwards to the actual implementation in scripts/fix_missing_dependencies.js
+ */
+
+// Simply require and execute the actual script
+require('./scripts/fix_missing_dependencies.js');
---
+++ heroku-deploy-final.js
@@ -0,0 +1,8 @@
+/**
+ * Root directory wrapper for heroku_deploy_final.js
+ * 
+ * This script forwards to the actual implementation in scripts/heroku_deploy_final.js
+ */
+
+// Simply require and execute the actual script
+require('./scripts/heroku_deploy_final.js');
